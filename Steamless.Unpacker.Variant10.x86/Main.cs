5c2c32cab4cf4ec3a5ec4596e3f60383b13c09e6
atom0s <atom0s@live.com>
2022-03-25 18:52:33 -0700

    Begin variant v1.0 x86 plugin support.

---
+++ Steamless.Unpacker.Variant10.x86/Main.cs
@@ -0,0 +1,137 @@
+﻿/**
+ * Steamless - Copyright (c) 2015 - 2022 atom0s [atom0s@live.com]
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/ or send a letter to
+ * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ *
+ * By using Steamless, you agree to the above license and its terms.
+ *
+ *      Attribution - You must give appropriate credit, provide a link to the license and indicate if changes were
+ *                    made. You must do so in any reasonable manner, but not in any way that suggests the licensor
+ *                    endorses you or your use.
+ *
+ *   Non-Commercial - You may not use the material (Steamless) for commercial purposes.
+ *
+ *   No-Derivatives - If you remix, transform, or build upon the material (Steamless), you may not distribute the
+ *                    modified material. You are, however, allowed to submit the modified works back to the original
+ *                    Steamless project in attempt to have it added to the original project.
+ *
+ * You may not apply legal terms or technological measures that legally restrict others
+ * from doing anything the license permits.
+ *
+ * No warranties are given.
+ */
+
+namespace Steamless.Unpacker.Variant10.x86
+{
+    using API;
+    using API.Events;
+    using API.Model;
+    using API.PE32;
+    using API.Services;
+    using System;
+    using System.Reflection;
+
+    [SteamlessApiVersion(1, 0)]
+    public class Main : SteamlessPlugin
+    {
+        /// <summary>
+        /// Internal logging service instance.
+        /// </summary>
+        private LoggingService m_LoggingService;
+
+        /// <summary>
+        /// Gets the author of this plugin.
+        /// </summary>
+        public override string Author => "atom0s";
+
+        /// <summary>
+        /// Gets the name of this plugin.
+        /// </summary>
+        public override string Name => "SteamStub Variant 1.0 Unpacker (x86)";
+
+        /// <summary>
+        /// Gets the description of this plugin.
+        /// </summary>
+        public override string Description => "Unpacker for the 32bit SteamStub variant 1.0.";
+
+        /// <summary>
+        /// Gets the version of this plugin.
+        /// </summary>
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        /// <summary>
+        /// Internal wrapper to log a message.
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="type"></param>
+        private void Log(string msg, LogMessageType type)
+        {
+            this.m_LoggingService.OnAddLogMessage(this, new LogMessageEventArgs(msg, type));
+        }
+
+        /// <summary>
+        /// Initialize function called when this plugin is first loaded.
+        /// </summary>
+        /// <param name="logService"></param>
+        /// <returns></returns>
+        public override bool Initialize(LoggingService logService)
+        {
+            this.m_LoggingService = logService;
+            return true;
+        }
+
+        /// <summary>
+        /// Processing function called when a file is being unpacked. Allows plugins to check the file
+        /// and see if it can handle the file for its intended purpose.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public override bool CanProcessFile(string file)
+        {
+            try
+            {
+                // Load the file..
+                var f = new Pe32File(file);
+                if (!f.Parse() || f.IsFile64Bit() || !f.HasSection(".bind"))
+                    return false;
+
+                // Obtain the bind section data..
+                var bind = f.GetSectionData(".bind");
+
+                // Attempt to locate the known v1.x signature..
+                var variant = Pe32Helpers.FindPattern(bind, "60 81 EC 00 10 00 00 BE ?? ?? ?? ?? B9 6A");
+                if (variant == -1)
+                    return false;
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Processing function called to allow the plugin to process the file.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="options"></param>
+        /// <returns></returns>
+        public override bool ProcessFile(string file, SteamlessOptions options)
+        {
+            // Parse the file..
+            this.File = new Pe32File(file);
+            if (!this.File.Parse())
+                return false;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gets or sets the file being processed.
+        /// </summary>
+        private Pe32File File { get; set; }
+    }
+}
