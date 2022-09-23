fa0d8319f32c6984655a32b067e210e7f6cbf413
atom0s <atom0s@live.com>
2022-09-23 16:04:00 -0700

    Bump version.

--- Steamless/Properties/AssemblyInfo.cs
+++ Steamless/Properties/AssemblyInfo.cs
@@ -37,5 +37,5 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyVersion("3.1.0.1")]
-[assembly: AssemblyFileVersion("3.1.0.1")] 
+[assembly: AssemblyVersion("3.1.0.2")]
+[assembly: AssemblyFileVersion("3.1.0.2")] 
